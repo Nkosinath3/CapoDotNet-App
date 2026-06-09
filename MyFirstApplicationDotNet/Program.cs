49d381a4ed36acbbc98af2b5451c89b57ef62918
Nkosinath3 <51296323+Nkosinath3@users.noreply.github.com>
2026-06-09 12:16:08 +0000

    Added first .NET console app

---
+++ MyFirstApplicationDotNet/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter username: ");
+        string username = Console.ReadLine();
+
+        Console.Write("Enter password: ");
+        string password = Console.ReadLine();
+
+        Console.WriteLine($"Welcome, {username}!");
+    }
+}
